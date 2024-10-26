39e00a9c96c6a82508a85f5a87f228b34e603059
Akash Verma <vermaakash04012002@gmail.com>
2024-10-26 10:00:54 +0530

    Updated the logos

--- src/User.tsx
+++ src/User.tsx
@@ -1,150 +1,186 @@
-import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube } from "@tabler/icons-react";
+import {
+    IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLeetcode,
+  IconBrandLinkedin,
+  IconBrandX,
+  IconBrandYoutube,
+} from '@tabler/icons-react';
+
 const Info = {
-    name: "Akash Verma",
-    stack: ["Software Engineer", "Full Stack Developer", "Competitive Programmer", "Freelancer", "Open Source Contributor"],
-    bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
-}
-
-
+  name: 'Akash Verma',
+  stack: [
+    'Software Engineer',
+    'Competetive Programmar',
+    'Full Stack Web Developer',
+    'Software Developer',
+    'Open Source Contributor',
+  ],
+  bio: 'I’m a PHP developer with 1.3 year of experience in creating dynamic web applications. I specialize in leveraging PHP and frameworks like Laravel to build efficient, user-friendly solutions. Passionate about clean code and innovative design, I enjoy collaborating with teams to bring ideas to life. When I’m not coding, I love exploring new technologies and contributing to open-source projects. Let’s connect!',
+};
 
 const ProjectInfo = [
-    {
-        title: "Ekart E-comm App",
-        desc: "Ekart is a modern, fully responsive e-commerce platform built using React, TailwindCSS, Heroicons, and HeadlessUI, offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
-        image: "Ekart.png",
-        live: true,
-        technologies: ["React", "Tailwind", "Redux", "Heroicons", "HeadlessUI"],
-        link: "https://code-mars.github.io/Ekart/",
-        github: "https://github.com/Code-Mars/Ekart/"
-    },
-    {
-        title: "Facebook Clone",
-        desc: "Facebook Clone is a full-stack web application that replicates the core features of Facebook, including user authentication, posting, and commenting. The front end is built with React, TailwinCSS, Tabler-Icons, Mantine and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, post updates, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Facebook Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Facebook.png",
-        live: false,
-        technologies: ["React", "Springboot", "MongoDB", "Tailwind", "Tabler-Icons", "Firebase"],
-        link: "https://github.com/Code-Mars/Facebook-Clone",
-        github: "https://github.com/Code-Mars/Facebook-Clone"
-    },
-    {
-        title: "Spotify Clone",
-        desc: "Spotify Clone is a full-stack web application that replicates the core features of Spotify, including user authentication, music playback, and playlist creation. The front end is built with React, TailwindCSS, Heroicons, and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, search for songs, and create playlists. The app features real-time updates, so users can see new songs as they are added. Spotify Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Spotify.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Tabler-Icons", "MongoDB", "Firebase"],
-        link: "https://github.com/Code-Mars/Spotify-Clone",
-        github: "https://github.com/Code-Mars/Spotify-Clone"
-    },
-    {
-        title: "Travel Tracker",
-        desc: "Travel Tracker is a full-stack web application that allows users to track their travel experiences and share them with others. The front end is built with React, Bootstrap, while the back end uses Node.js, Express, and PostgresSQL. Users can create an account, log in, add new trips, and view their past trips on a map. The app features a clean, modern design with interactive maps and a user-friendly interface. Travel Tracker combines functionality with style, making it easy and enjoyable to record and share travel memories.",
-        image: "Travel.png",
-        live: false,
-        technologies: ["React", "Node.js", "PostgresSQL", "Express", "Bootstrap"],
-        link: "https://github.com/Code-Mars/Travel-Tracker",
-        github: "https://github.com/Code-Mars/Travel-Tracker"
-    },
-    {
-        title: "Instagram Clone",
-        desc: "Instagram Clone is a full-stack web application that replicates the core features of Instagram, including user authentication, image uploading, and commenting. The front end is built with React, TailwindCSS, and Heroicons, while the back end uses Firebase for user authentication and image storage. Users can create an account, log in, upload images, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Instagram Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Instagram.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Heroicons", "Firebase", "MongoDB"],
-        link: "https://github.com/Code-Mars/Instagram-Clone",
-        github: "https://github.com/Code-Mars/Instagram-Clone"
-    },
-    {
-        title: "CodeX Code Editor",
-        desc: "CodeX is a modern, fully responsive code editor built using React, TailwindCSS, and Ace Editor, offering a seamless coding experience across all devices. It features syntax highlighting for popular programming languages, including HTML, CSS, JavaScript, Python, and Java. Users can create, edit, and save code files, with the option to download them as text files. CodeX combines robust functionality with a sleek, intuitive design, making coding effortless and enjoyable.",
-        image: "CodeX.png",
-        live: false,
-        technologies: ["React", "Tailwind", "Ace Editor"],
-        link: "https://github.com/Code-Mars/CodeX",
-        github: "https://github.com/Code-Mars/CodeX"
-    }
-]
-
+  {
+    title: 'Norway Fjord',
+    desc: 'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+    image:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
+    live: true,
+    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Mantine'],
+    link: 'https://norway-fjord-adventures.vercel.app/',
+    github: 'https://github.com',
+  },
+  {
+    title: 'Norway Fjord',
+    desc: 'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+    image:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
+    live: false,
+    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Mantine'],
+    link: 'https://norway-fjord-adventures.vercel.app/',
+    github: 'https://github.com',
+  },
+  {
+    title: 'Norway Fjord',
+    desc: 'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+    image:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
+    live: true,
+    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Mantine'],
+    link: 'https://norway-fjord-adventures.vercel.app/',
+    github: 'https://github.com',
+  },
+  {
+    title: 'Norway Fjord',
+    desc: 'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+    image:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
+    live: false,
+    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Mantine'],
+    link: 'https://norway-fjord-adventures.vercel.app/',
+    github: 'https://github.com',
+  },
+  {
+    title: 'Norway Fjord',
+    desc: 'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+    image:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
+    live: true,
+    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Mantine'],
+    link: 'https://norway-fjord-adventures.vercel.app/',
+    github: 'https://github.com',
+  },
+  {
+    title: 'Norway Fjord',
+    desc: 'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+    image:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png',
+    live: false,
+    technologies: ['React', 'TypeScript', 'Tailwind CSS', 'Mantine'],
+    link: 'https://norway-fjord-adventures.vercel.app/',
+    github: 'https://github.com',
+  },
+];
 
 const SkillInfo = [
-    {
-        title: "Frontend",
-        skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Angular", "Redux", "Tailwind CSS", "GSAP", "Material UI", "Bootstrap"
-        ]
-    },
-    {
-        title: "Backend",
-        skills: ["Springboot", "Node JS", "Express JS", "MySQL", "MongoDB", "Firebase", "PostgresSQL"]
-    },
-    {
-        title: "Languages",
-        skills: ["C", "C++", "Java", "JavaScript", "TypeScript"]
-    },
-    {
-        title: "Tools",
-        skills: ["Git", "Github", "VS Code", "Postman", "MongoDB Compass", "Spring Tool Suite"]
-    }
-]
-const socialLinks = [
-    { link: "https://github.com/akashverma004", icon: IconBrandGithub },
-    { link: "https://www.linkedin.com/in/akashverma004", icon: IconBrandLinkedin },
-    { link: "https://www.instagram.com/", icon: IconBrandInstagram }, 
-    { link: "https://www.youtube.com/", icon: IconBrandYoutube },
-    { link: "https://www.leetcode.com/", icon: IconBrandLeetcode }
+  {
+    title: 'Frontend',
+    skills: [
+      'HTML',
+      'CSS',
+      'javaScript',
+      'React JS',
+      'Tailwind CSS',
+      'Bootstrap',
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      'Laravel',
+      'Yii2',
+      'Node JS',
+      'Express JS',
+      'MySQL',
+      'MongoDB',
+      'Firebase',
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: ['C', 'Java', 'PHP', 'Python', 'JavaScript'],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      'Git',
+      'GitHub',
+      'VS Code',
+      'Eclipse',
+      'Postman',
+      'PhpMyAdmin',
+      'MS Office',
+    ],
+  },
 ];
 
+const socialLinks = [
+    { link: 'https://github.com/akashverma004', icon: IconBrandGithub },
+    { link: 'https://linkedin.com/in/akashverma004', icon: IconBrandLinkedin },
+    { link: 'https://leetcode.com/u/akash_verma_004/', icon: IconBrandLeetcode },
+    { link: 'https://www.instagram.com/__akash_004', icon: IconBrandInstagram },
+    { link: 'https://youtube.com/', icon: IconBrandYoutube },
+  ];
 
 const ExperienceInfo = [
-    {
-        role: "Specialist Programmer",
-        company: "Infosys",
-        date: "Oct 2023 - Present",
-        desc: "I led software development with Spring Boot, React, and Angular, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
-        skills: ["Springboot", "React JS", "Angular", "Node JS", "MySQL", "MongoDB", "Microservices"]
-    },
-    {
-        role: "System Engineer",
-        company: "Infosys",
-        date: "Nov 2022 - Sep 2023",
-        desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
-        skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
-    }
-]
-const Slugs = [
-    "typescript",
-    "spring",
-    "javascript",
-    "dart",
-    "java",
-    "react",
-    "angular",
-    "flutter",
-    "android",
-    "html5",
-    "css3",
-    "springboot",
-    "mongodb",
-    "selenium",
-    "nodedotjs",
-    "express",
-    "nextdotjs",
-    "prisma",
-    "mysql",
-    "amazonaws",
-    "postgresql",
-    "firebase",
-    "nginx",
-    "vercel",
-    "testinglibrary",
-    "jest",
-    "cypress",
-    "docker",
-    "git",
-    "jira",
-    "github",
-    "gitlab",
-    "visualstudiocode",
-    "androidstudio",
-    "sonarqube",
-    "figma",
+  {
+    role: 'Software Developer',
+    company: 'ToXSL Technologies Pvt Ltd',
+    date: 'March 2024 - Present',
+    desc: 'Leveraged PHP Frameworks like Laravel and Yii2 and resources like composer for efficient backend operation. Conducted API implementation, fetching, mapping and posting of data including to different APIs, endpoints for PUT, GET, POST and DELETE methods.',
+    skills: ['PHP', 'Yii2', 'Laravel', 'Bootstrap', 'MySQL'],
+  },
+  {
+    role: 'Full Stack Developer Trainee',
+    company: 'Geeta Pariwar',
+    date: 'June 2023 - March 2024',
+    desc: 'Developed and maintained full stack Ed-Tech web application using PHP and MySQL, focusing user-centric layout views. Worked on user management, exam management and content management portals. Designed and developed their multiple online portals which increases their global presence.',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'PHP', 'MySQL'],
+  },
 ];
-export { Info, ProjectInfo,socialLinks, SkillInfo, ExperienceInfo, Slugs };
+
+const Slugs = [
+  'typescript',
+  'javascript',
+  'dart',
+  'java',
+  'react',
+  'flutter',
+  'android',
+  'html5',
+  'css3',
+  'nodedotjs',
+  'express',
+  'nextdotjs',
+  'prisma',
+  'amazonaws',
+  'postgresql',
+  'firebase',
+  'nginx',
+  'vercel',
+  'testinglibrary',
+  'jest',
+  'cypress',
+  'docker',
+  'git',
+  'jira',
+  'github',
+  'gitlab',
+  'visualstudiocode',
+  'androidstudio',
+  'sonarqube',
+  'figma',
+];
+
+export { Info, ProjectInfo, socialLinks, SkillInfo, ExperienceInfo, Slugs };
